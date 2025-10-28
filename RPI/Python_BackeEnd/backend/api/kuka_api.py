6a4ecb020eb56d710abd01cc2e8e4aa68588db41
Dalik0002 <daliborsliva20@gmail.com>
2025-10-28 16:08:41 +0100

    Kuka api modified

--- RPI/Python_BackeEnd/backend/api/kuka_api.py
+++ RPI/Python_BackeEnd/backend/api/kuka_api.py
@@ -8,9 +8,9 @@ router_kuka = APIRouter(prefix="/Kuka", tags=["Kuka"])
 @router_kuka.get("/robot/status")
 def status():
     return {
-        "connected": robot.is_connected(),
-        "ip": getattr(robot, "_ip", None),
-        "port": getattr(robot, "_port", None),
+        "connected": robot.KUKA_IsConnected(),
+        "ip": robot.ipAddress,
+        "port": robot.port,
     }
     
 @router_kuka.post("/robot/connect")
@@ -20,7 +20,7 @@ def connect():
         if not ok:
             raise HTTPException(500, "Open() failed")
 
-        client = robot._client
+        client = robot.client
         if not client or not client.can_connect():
             robot.close()
             raise HTTPException(502, f"Cannot connect to KUKA at {robot.ipAddress}:{robot.port}")
@@ -29,14 +29,14 @@ def connect():
         return {"connected": True, "ip": robot.ipAddress, "port": robot.port}
 
     except Exception as e:
-        robot.close()
+        robot.KUKA_Close()
         raise HTTPException(500, f"Connect exception: {e}")
 
 
 @router_kuka.post("/robot/disconnect")
 def disconnect():
     try:
-        robot.close()
+        robot.KUKA_Close()
         print(f"[KUKA] Disconnected from robot")
         return {"connected": False}
     
@@ -47,7 +47,7 @@ def disconnect():
 
 @router_kuka.post("/robot/playSong")
 def play_song(song: int = Query(...)):
-    if not robot.is_connected():
+    if not robot.KUKA_IsConnected():
         raise HTTPException(400, "Not connected")
     ok = robot.play_song(song_number=song)
     if not ok:
